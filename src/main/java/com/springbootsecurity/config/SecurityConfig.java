379365b9aa1922207e88d8afadc896682f1550b1
vijay <vijay.durgade@gmail.com>
2023-10-17 17:54:44 +0530

    Initial commit

---
+++ src/main/java/com/springbootsecurity/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.springbootsecurity.config;
+
+import com.springbootsecurity.service.UserInfoUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        /*UserDetails admin = User.withUsername("Vijay")
+                .password(encoder.encode("Pwd1"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user = User.withUsername("Vansh")
+                .password(encoder.encode("Pwd2"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(admin,user);*/
+
+        return new UserInfoUserDetailsService();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/products/welcome", "/products/new").permitAll()
+                .and()
+                .authorizeHttpRequests().requestMatchers("/products/**")
+                .authenticated().and().formLogin().and().build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+
+    }
+}
